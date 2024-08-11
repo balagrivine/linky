5fd22fdc7bcbb43978eeb3ef9606ed013bb76f1a
balagrivine <balagrivine@gmail.com>
2024-08-11 10:09:39 -0700

    add feature to create user using email

--- logic/handle_create_url.go
+++ logic/handle_create_url.go
@@ -13,12 +13,48 @@ import (
 	"github.com/balagrivine/linky/internal/database"
 )
 
+type createUser struct {
+	Email string `json:"email" validate:"required"`
+}
+
+func handleCreateUser(dbCfg *config.DBConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		var param createUser
+
+		// Decode thr request body into the param variable
+		err := decoder.Decode(&param)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error parsong JSON: %s", err), http.StatusBadRequest)
+			return
+		}
+
+		validate := validator.New()
+
+		err := validate.Struct(param)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Validation failed: %s", err), http.StatusBadRequest)
+			return
+		}
+
+		user, err := dbCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+			Email: param.Email
+		})
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error creating user: %s", err), http.InternalServerError)
+			return
+		}
+
+		json.NewEncoder(w).Encode(user)
+	}
+}
+
 type createURL struct {
 	OriginalURL  string `json:"original_url" validate:"required"`
 	CreatedAt    string `json:"created_at" validate:"required"`
 }
 
-func CreateShortURL(apiCfg *config.APIConfig) http.HandlerFunc {
+func CreateShortURL(apiCfg *config.DBConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request){
 		decoder := json.NewDecoder(r.Body)
 		var param CreateURL
